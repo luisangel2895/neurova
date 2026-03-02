1f465a88b5ab4f1f7f1f37abaac62f99ebcf0d03
Angel Orellana <luisangel2895@gmail.com>
2026-03-02 04:21:08 -0500

    Initial Commit

---
+++ Neurova/NeurovaApp.swift
@@ -0,0 +1,32 @@
+//
+//  NeurovaApp.swift
+//  Neurova
+//
+//  Created by Angel Orellana on 2/03/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct NeurovaApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ NeurovaTests/NeurovaTests.swift
@@ -0,0 +1,16 @@
+//
+//  NeurovaTests.swift
+//  NeurovaTests
+//
+//  Created by Angel Orellana on 2/03/26.
+//
+
+import Testing
+
+struct NeurovaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
